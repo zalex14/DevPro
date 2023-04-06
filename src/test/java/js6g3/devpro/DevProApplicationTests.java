b771c85ae4798beec3d608b6e0b2bfb15d50d679
zalex14 <9824018@gmail.com>
2023-04-06 15:52:20 +0300

    Spring-Boot приложение, интегрированное с телеграмм-ботом

---
+++ src/test/java/js6g3/devpro/DevProApplicationTests.java
@@ -0,0 +1,13 @@
+package js6g3.devpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
